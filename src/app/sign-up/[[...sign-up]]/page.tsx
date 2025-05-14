0ec59e0b2d028ab76590ae97a432e1082aefbb58
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-14 19:16:28 +0530

    Add sign-in and sign-up pages with Clerk integration

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+	return (
+		<div className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2">
+			<SignUp />
+		</div>
+	);
+}
