8138a078faeaf1d2ddeef207505c0ebc9f5ef98d
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-19 22:02:30 +0530

    Refactor utils.ts for consistent formatting and enhance convertToASCII function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function convertToASCII(input: string) {
+	// remove non ascii characters
+	const asciiString = input.replace(/[^\x00-\x7F]+/g, "");
+
+	return asciiString;
 }
