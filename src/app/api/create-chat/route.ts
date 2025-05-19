71229f05b09905d09a44bc79de0005802cdb1cde
Mohit Soni <developer_mohitsoni@outlook.com>
2025-05-19 22:03:37 +0530

    Refactor create-chat API to include user authentication and return chat ID upon successful insertion

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,7 +1,16 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 import { loadS3IntoPinecone } from "@/lib/pinecone";
+import { getS3URL } from "@/lib/s3";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+	const { userId } = await auth();
+
+	if (!userId) {
+		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+	}
 	try {
 		const body = await req.json();
 		const { file_key, file_name } = body;
@@ -16,9 +25,26 @@ export async function POST(req: Request) {
 		console.log("file_key", file_key);
 		console.log("file_name", file_name);
 
-		const pages = await loadS3IntoPinecone(file_key);
+		await loadS3IntoPinecone(file_key);
 
-		return NextResponse.json({ pages });
+		const chat_id = await db
+			.insert(chats)
+			.values({
+				fileKey: file_key,
+				pdfName: file_name,
+				pdfUrl: getS3URL(file_key),
+				userId
+			})
+			.returning({
+				insertedId: chats.id
+			});
+
+		return NextResponse.json(
+			{
+				chat_id: chat_id[0].insertedId
+			},
+			{ status: 200 }
+		);
 	} catch (err) {
 		console.error("Error in create-chat API", err);
 		return NextResponse.json(
